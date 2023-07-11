5ecea0bf856163adffd29688bca8b5fd0a8ea9e4
Jennifer <jng-96@hotmail.co.uk>
2023-07-11 13:50:41 +0100

    first api fetch working

--- front_end/src/components/SearchForm.js
+++ front_end/src/components/SearchForm.js
@@ -1,22 +1,26 @@
 import { useState } from "react"
+import ExternalServices from "../services/ExternalServices"
 
 const SearchForm = () => {
 
     const [searchQuery, setSearchQuery] = useState("")
     const [searchCategory, setSearchCategory] = useState()
 
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
     const handleChange = (event) => {
         setSearchQuery(event.target.value)
+        ExternalServices.getFilmByTitle(event.target.value)
     }
 
     return (
         <div>
-            <p>search Form</p>
-    
-        <form>
-            
-            <input type="text" placeholder="search" onChange={handleChange} value={searchQuery}></input>
-        </form>
+            <form onSubmit = {handleSubmit}> 
+                <input type="text" placeholder="search" onChange={handleChange} value={searchQuery}></input>
+            </form>
         </div>
     )
 
