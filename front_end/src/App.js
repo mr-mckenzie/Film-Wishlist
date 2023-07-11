f03b3d878b24c806db2f53f3d8e22def32851ca6
M <mrgmckenzie@gmail.com>
2023-07-11 12:59:10 +0100

    created form and search bar

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import './App.css';
+import SearchForm from './components/SearchForm';
 
 function App() {
+
+  // const [searchQuery, setSearchQuery] = useState("")
+  // const [searchCategory, setSearchCategory] = useState()
+
   return (
     <div className="App">
-
+      
+      <SearchForm/>
+      <p>App level para</p>
+    
     </div>
   );
 }
