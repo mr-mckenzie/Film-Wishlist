6f56b10aaa4cd16ce209e2caedb5712c486ceb1a
Jennifer <jng-96@hotmail.co.uk>
2023-07-12 17:22:20 +0100

    code refactored for neatness

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,9 +1,6 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Fullpage,{ FullPageSections, FullpageSection, FullpageNavigation } from "@ap.cx/react-fullpage";
 import './App.css';
-import FullPageScroll from './FullPageScroll';
-
-import NavBar from './components/NavBar';
 
 import Home from './pages/Home';
 import Search from './pages/Search';
@@ -11,43 +8,51 @@ import Recommendations from './pages/Recommendations';
 import Wishlist from './pages/Wishlist';
 import Ratings from './pages/Ratings';
 import Statistics from './pages/Statistics';
-import ErrorPage from './pages/ErrorPage';
-
 
 
 function App() {
 
-  // const [searchQuery, setSearchQuery] = useState("")
-  // const [searchCategory, setSearchCategory] = useState()
   const [films, setFilms] = useState([])
   const [wishlist, setWishlist] = useState([])
   const [selectedFilm, setSelectedFilm] = useState({})
 
+  const SectionStyle = {
+      // input height/width/display etc as needed
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+  }
+
   return (
-
-    <FullPageScroll 
-    setFilms={setFilms} 
-    films={films} 
-    wishlist={wishlist} 
-    setWishlist={setWishlist}
-    selectedFilm={selectedFilm}
-    setSelectedFilm={setSelectedFilm}
-    />
-    // <Router>
-    //   <NavBar/>
-    //   {/* <div className="App"> */}
-    //   <Routes>
-    //     <Route exact path = "/" element = {<Home/>} />
-    //     <Route path = "/search" element = {<Search/>} />
-    //     <Route path = "/recommendations" element = {<Recommendations/>} />
-    //     <Route path = "/wishlist" element = {<Wishlist/>} />
-    //     <Route path = "/ratings" element = {<Ratings/>} />
-    //     <Route path = "/statistics" element = {<Statistics/>} />
-    //     <Route path = "*" element = {<ErrorPage/>} />
-    //   </Routes>
-    // </Router>
-
-  );
+    <Fullpage>
+        <FullpageNavigation/>
+        <FullPageSections>
+            <FullpageSection style={SectionStyle}>
+                <Home/>
+            </FullpageSection>
+            <FullpageSection style={SectionStyle}>
+                <Search 
+                films={films} 
+                setFilms={setFilms} 
+                setSelectedFilm={setSelectedFilm}/>
+            </FullpageSection>
+            <FullpageSection style={SectionStyle}>
+                <Recommendations/>
+            </FullpageSection>
+            <FullpageSection style={SectionStyle}>
+                <Wishlist 
+                wishlist={wishlist} 
+                setWishlist={setWishlist}/>
+            </FullpageSection>
+            <FullpageSection style={SectionStyle}>
+                <Ratings/>
+            </FullpageSection>
+            <FullpageSection style={SectionStyle}>
+                <Statistics/>
+            </FullpageSection>
+        </FullPageSections>
+    </Fullpage>
+  )
 }
 
 export default App;
