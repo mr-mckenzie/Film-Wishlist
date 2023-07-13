6cd8cd01555cda855b031db074982aa10c8b413b
Jennifer <jng-96@hotmail.co.uk>
2023-07-13 14:59:22 +0100

    backend deleted

--- back_end/filmService/src/main/java/com/film_project/filmService/controllers/GenreController.java
+++
@@ -1,7 +0,0 @@
-package com.film_project.filmService.controllers;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class GenreController {
-}
--- back_end/filmService/src/main/java/com/film_project/filmService/repositories/CastRepository.java
+++
@@ -1,9 +0,0 @@
-package com.film_project.filmService.repositories;
-
-import com.film_project.filmService.models.Film;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CastRepository extends JpaRepository<Cast, Long> {
-}
