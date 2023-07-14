41c9c85f7e31cf2ad7392d230d4dbf0d4241c750
Jennifer <jng-96@hotmail.co.uk>
2023-07-14 14:56:55 +0100

    films can be rated

--- front_end/src/pages/Search.js
+++ front_end/src/pages/Search.js
@@ -2,11 +2,11 @@ import React from "react";
 import SearchForm from '../components/Search_SearchForm';
 import SearchResults from '../components/Search_SearchResults';
 
-const Search = ({listOfFilmsFromAPI, setListOfFilmsFromAPI, setSelectedFilm, wishlist, setWishlist}) => (
+const Search = ({listOfFilmsFromAPI, setListOfFilmsFromAPI, wishlist, setWishlist}) => (
     <div>
         <h4>Search</h4>
         <SearchForm setListOfFilmsFromAPI={setListOfFilmsFromAPI}/> 
-        <SearchResults listOfFilmsFromAPI={listOfFilmsFromAPI} setSelectedFilm={setSelectedFilm} wishlist={wishlist} setWishlist={setWishlist}/>
+        <SearchResults listOfFilmsFromAPI={listOfFilmsFromAPI} wishlist={wishlist} setWishlist={setWishlist}/>
     </div>
 )
 
