82aa583bdd1f370e51bc967e28fcdd9dc20c6920
Jennifer <jng-96@hotmail.co.uk>
2023-07-19 11:52:39 +0100

    hover is working

--- front_end/src/pages/unused/Recommendations.js
+++ front_end/src/pages/unused/Recommendations.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import './Recommendations.css'
+import DeleteLater from "../../components/unused/DeleteLater_RecsFilms";
 const Recommendations = () => (
-    <h4>Recommendations</h4>
+    <div class="recommendations_page">
+        <h1>Recommendations</h1>
+        <DeleteLater/>
+    </div>
 )
 
 export default Recommendations
