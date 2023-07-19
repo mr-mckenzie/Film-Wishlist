33cc9f54aac508c1ce767a8aa39035f7febfb0d0
M <mrgmckenzie@gmail.com>
2023-07-19 15:12:15 +0100

    updated search page to have actor avatars

--- front_end/src/services/StatisticsFunctions.js
+++ front_end/src/services/StatisticsFunctions.js
@@ -14,20 +14,20 @@ const StatisticsFunctions = {
             for (const film of list) {
                 if (secondField) {
                     for (const element of film[field][secondField]) {
-                        //MAYBE CHANGE THIS TO ACTOR ID INSTEAD OF NAME?
+                        //MAYBE CHANGE THIS TO ID INSTEAD OF NAME?
                         if (counterObject[element.name]) {
-                            counterObject[element.name].push(film.rating)
+                            counterObject[element.name][0].push(film.rating)
                         } else {
-                            counterObject[element.name] = [film.rating]
+                            counterObject[element.name] = [[film.rating], [element["profile_path"]]]
                         }
                     }
                 } else {
                 for (const element of film[field]) {
-                    //MAYBE CHANGE THIS TO ACTOR ID INSTEAD OF NAME?
+                    //MAYBE CHANGE THIS TO ID INSTEAD OF NAME?
                     if (counterObject[element.name]) {
-                        counterObject[element.name].push(film.rating)
+                        counterObject[element.name][0].push(film.rating)
                     } else {
-                        counterObject[element.name] = [film.rating]
+                        counterObject[element.name] = [[film.rating],[element["profile_path"]]]
                     }
                 }
             }
@@ -61,223 +61,156 @@ const StatisticsFunctions = {
         return this.getFieldFromFilmList(list, "spoken_languages")
     },
 
-        // getActorsInList (list) {
-        //     let actorCounterObject = {}
-        //     if (list.length > 0){
-        //         for (const film of list) {
-        //             for (const castMember of film.credits.cast) {
-        //                 //MAYBE CHANGE THIS TO ACTOR ID INSTEAD OF NAME?
-        //                 if (actorCounterObject[castMember.name]) {
-        //                     actorCounterObject[castMember.name].push(film.rating)
-        //                 } else {
-        //                     actorCounterObject[castMember.name] = [film.rating]
-        //                 }
-        //             }
-        //         }
+    sortByRating (object) {
 
-        //         for (const [key, value] of Object.entries(actorCounterObject)) {
-        //             if (value.length == 1) {
-        //                 delete actorCounterObject[key]
-        //             }
-        //         }
-        //     }
-        //     return actorCounterObject
-        // },
+        const arrayForSorting = [];
+        for (const [key, value] of Object.entries(object)) {
+            //console.log("key", key)
+            //console.log("value", value)
 
-        // getGenresInList (list) {
-        //     let genreCounterObject = {}
-        //     for (const film of list) {
-        //         for (const genre of film.genres) {
-        //             //MAYBE CHANGE THIS TO GENRE ID INSTEAD OF NAME?
-        //             if (genreCounterObject[genre.name]) {
-        //                 genreCounterObject[genre.name].push(film.rating)
-        //             } else {
-        //                 genreCounterObject[genre.name] = [film.rating]
-                        
-        //             }
-        //         }
-        //     }
+            const numberOfRatings = value[0].length
 
-        //     for (const [key, value] of Object.entries(genreCounterObject)) {
-        //         if (value.length == 1) {
-        //             delete genreCounterObject[key]
-        //         }
-        //     }
-        //     return genreCounterObject
-        // },
-
-        // getKeywordsInList (list) {
-        //     let genreCounterObject = {}
-        //     for (const film of list) {
-        //         for (const genre of film.keywords.keywords) {
-        //             //MAYBE CHANGE THIS TO GENRE ID INSTEAD OF NAME?
-        //             if (genreCounterObject[genre.name]) {
-        //                 genreCounterObject[genre.name].push(film.rating)
-        //             } else {
-        //                 genreCounterObject[genre.name] = [film.rating]
-                        
-        //             }
-        //         }
-        //     }
-
-        //     for (const [key, value] of Object.entries(genreCounterObject)) {
-        //         if (value.length == 1) {
-        //             delete genreCounterObject[key]
-        //         }
-        //     }
-        //     return genreCounterObject
-        // },
-
-        sortByRating (object) {
-
-            const arrayForSorting = [];
-            for (const [key, value] of Object.entries(object)) {
-                //console.log("key", key)
-                //console.log("value", value)
-
-                const numberOfRatings = value.length
-
-                let sumOfRatings = 0
-                for (let i = 0; i<value.length; i++) {
-                    sumOfRatings = sumOfRatings + value[i]
-                }
-
-                const averageRating = sumOfRatings/numberOfRatings
-
-                //key = name, value = array of individual ratings
-                arrayForSorting.push([key, value, numberOfRatings, averageRating]);
-            }
-            
-            //console.log("Array for Sorting:", arrayForSorting)
-
-            const sortingFunction = (a, b) => {
-                //first sort by rating
-                if (a[3] > b[3]) {
-                    return -1
-                } else if (a[3] < b[3]) {
-                    return 1
-                //then sort by number of films watched
-                } else if (a[2] > b[2]) {
-                    return -1 
-                } else if (a[2] < b[2]) {
-                    return 1
-                //then sort alphabetically
-                } else if (a[0] > b[0]) {
-                    return -1 
-                } else if (a[0] < b[0]) {
-                    return 1
-                } else {
-                    return 0
-                }
+            let sumOfRatings = 0
+            for (let i = 0; i<value[0].length; i++) {
+                sumOfRatings = sumOfRatings + value[0][i]
             }
 
-            let sortedArray = arrayForSorting.sort(sortingFunction)
+            const averageRating = sumOfRatings/numberOfRatings
 
-            return sortedArray
-        },
-
-        sortByMostWatched (object) {
-
-            const arrayForSorting = [];
-            for (const [key, value] of Object.entries(object)) {
-                //console.log("key", key)
-                //console.log("value", value)
-
-                const numberOfRatings = value.length
-
-                let sumOfRatings = 0
-                for (let i = 0; i<value.length; i++) {
-                    sumOfRatings = sumOfRatings + value[i]
-                }
-
-                const averageRating = sumOfRatings/numberOfRatings
-
-                //key = name, value = array of individual ratings
-                arrayForSorting.push([key, value, numberOfRatings, averageRating]);
-            }
-            
-            //console.log("Array for Sorting:", arrayForSorting)
-
-            const sortingFunction = (a, b) => {
-                // sort by most watched
-                if (a[2] > b[2]) {
-                    return -1
-                } else if (a[2] < b[2]) {
-                    return 1
-                // then sort by higest rated
-                } else if (a[3] > b[3]) {
-                    return -1 
-                } else if (a[3] < b[3]) {
-                    return 1
-                } else {
-                    return 0
-                }
-            }
-
-            let sortedArray = arrayForSorting.sort(sortingFunction)
-
-            return sortedArray
-        },
-
-        getArrayOfGenresByRating(list) {
-            const genresObject = this.getGenresInList(list)
-            const arrayOfGenresSortedByRating = this.sortByRating(genresObject)
-            return arrayOfGenresSortedByRating
-        },
-
-        getArrayOfGenresByMostWatched(list) {
-            const genresObject = this.getGenresInList(list)
-            const arrayOfGenresSortedByMostWatched = this.sortByMostWatched(genresObject)
-            return arrayOfGenresSortedByMostWatched
-        },
-
-        getArrayOfActorsByRating (list) {
-            const actorsObject = this.getActorsInList(list)
-            const arrayOfActorsSortedByRating = this.sortByRating(actorsObject)
-            return arrayOfActorsSortedByRating
-        },
-
-        getArrayOfActorsByMostWatched (list) {
-            const actorsObject = this.getActorsInList(list)
-            const arrayOfActorsSortedByMostWatched = this.sortByMostWatched(actorsObject)
-            return arrayOfActorsSortedByMostWatched
-        },
-
-        getArrayOfKeywordsByRating (list) {
-            const keywordsObject = this.getKeywordsInList(list)
-            const arrayOfKeywordsSortedByRating = this.sortByRating(keywordsObject)
-            return arrayOfKeywordsSortedByRating
-        },
-
-        getArrayOfKeywordsByMostWatched (list) {
-            const keywordsObject = this.getKeywordsInList(list)
-            const arrayOfKeywordsSortedByMostWatched = this.sortByMostWatched(keywordsObject)
-            return arrayOfKeywordsSortedByMostWatched
-        },
-
-        getArrayOfCountriesByRating (list) {
-            const countriesObject = this.getProductionsCountriesInList(list)
-            const arrayOfCountriesSortedByRating = this.sortByRating(countriesObject)
-            return arrayOfCountriesSortedByRating
-        },
-
-        getArrayOfCountriesByMostWatched (list) {
-            const countriesObject = this.getProductionsCountriesInList(list)
-            const arrayOfCountriesSortedByMostWatched = this.sortByMostWatched(countriesObject)
-            return arrayOfCountriesSortedByMostWatched
-        },
+            //key = name, value = array of individual ratings
+            arrayForSorting.push([key, value[0], numberOfRatings, averageRating, value[1]]);
+        }
         
-        getSpokenLanguagesByRating (list) {
-            const languagesObject = this.getSpokenLanguagesInList(list)
-            const languagesSortedByRating = this.sortByRating(languagesObject)
-            return languagesSortedByRating
-        }, 
+        //console.log("Array for Sorting:", arrayForSorting)
 
-        getSpokenLanguagesByMostWatched (list) {
-            const languagesObject = this.getSpokenLanguagesInList(list)
-            const languagesSortedByMostWatched = this.sortByMostWatched(languagesObject)
-            return languagesSortedByMostWatched
-        }, 
+        const sortingFunction = (a, b) => {
+            //first sort by rating
+            if (a[3] > b[3]) {
+                return -1
+            } else if (a[3] < b[3]) {
+                return 1
+            //then sort by number of films watched
+            } else if (a[2] > b[2]) {
+                return -1 
+            } else if (a[2] < b[2]) {
+                return 1
+            //then sort alphabetically
+            } else if (a[0] > b[0]) {
+                return -1 
+            } else if (a[0] < b[0]) {
+                return 1
+            } else {
+                return 0
+            }
+        }
+
+        let sortedArray = arrayForSorting.sort(sortingFunction)
+
+        return sortedArray
+    },
+
+    sortByMostWatched (object) {
+
+        const arrayForSorting = [];
+        for (const [key, value] of Object.entries(object)) {
+            //console.log("key", key)
+            //console.log("value", value)
+
+            const numberOfRatings = value[0].length
+
+            let sumOfRatings = 0
+            for (let i = 0; i<value[0].length; i++) {
+                sumOfRatings = sumOfRatings + value[0][i]
+            }
+
+            const averageRating = sumOfRatings/numberOfRatings
+
+            //key = name, value = array of individual ratings
+            arrayForSorting.push([key, value[0], numberOfRatings, averageRating, value[1]]);
+        }
+        
+        //console.log("Array for Sorting:", arrayForSorting)
+
+        const sortingFunction = (a, b) => {
+            // sort by most watched
+            if (a[2] > b[2]) {
+                return -1
+            } else if (a[2] < b[2]) {
+                return 1
+            // then sort by higest rated
+            } else if (a[3] > b[3]) {
+                return -1 
+            } else if (a[3] < b[3]) {
+                return 1
+            } else {
+                return 0
+            }
+        }
+
+        let sortedArray = arrayForSorting.sort(sortingFunction)
+
+        return sortedArray
+    },
+
+    getArrayOfGenresByRating(list) {
+        const genresObject = this.getGenresInList(list)
+        const arrayOfGenresSortedByRating = this.sortByRating(genresObject)
+        return arrayOfGenresSortedByRating
+    },
+
+    getArrayOfGenresByMostWatched(list) {
+        const genresObject = this.getGenresInList(list)
+        const arrayOfGenresSortedByMostWatched = this.sortByMostWatched(genresObject)
+        return arrayOfGenresSortedByMostWatched
+    },
+
+    getArrayOfActorsByRating (list) {
+        const actorsObject = this.getActorsInList(list)
+        const arrayOfActorsSortedByRating = this.sortByRating(actorsObject)
+        return arrayOfActorsSortedByRating
+    },
+
+    getArrayOfActorsByMostWatched (list) {
+        const actorsObject = this.getActorsInList(list)
+        const arrayOfActorsSortedByMostWatched = this.sortByMostWatched(actorsObject)
+        return arrayOfActorsSortedByMostWatched
+    },
+
+    getArrayOfKeywordsByRating (list) {
+        const keywordsObject = this.getKeywordsInList(list)
+        const arrayOfKeywordsSortedByRating = this.sortByRating(keywordsObject)
+        return arrayOfKeywordsSortedByRating
+    },
+
+    getArrayOfKeywordsByMostWatched (list) {
+        const keywordsObject = this.getKeywordsInList(list)
+        const arrayOfKeywordsSortedByMostWatched = this.sortByMostWatched(keywordsObject)
+        return arrayOfKeywordsSortedByMostWatched
+    },
+
+    getArrayOfCountriesByRating (list) {
+        const countriesObject = this.getProductionsCountriesInList(list)
+        const arrayOfCountriesSortedByRating = this.sortByRating(countriesObject)
+        return arrayOfCountriesSortedByRating
+    },
+
+    getArrayOfCountriesByMostWatched (list) {
+        const countriesObject = this.getProductionsCountriesInList(list)
+        const arrayOfCountriesSortedByMostWatched = this.sortByMostWatched(countriesObject)
+        return arrayOfCountriesSortedByMostWatched
+    },
+    
+    getSpokenLanguagesByRating (list) {
+        const languagesObject = this.getSpokenLanguagesInList(list)
+        const languagesSortedByRating = this.sortByRating(languagesObject)
+        return languagesSortedByRating
+    }, 
+
+    getSpokenLanguagesByMostWatched (list) {
+        const languagesObject = this.getSpokenLanguagesInList(list)
+        const languagesSortedByMostWatched = this.sortByMostWatched(languagesObject)
+        return languagesSortedByMostWatched
+    }, 
 
 
 }
