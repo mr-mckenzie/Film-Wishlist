0cd228f97a252bb1199d69ac88ce4517a8d5c546
M <mrgmckenzie@gmail.com>
2023-11-03 15:43:55 +0000

    implemented new wishlist button and added a few ternarys to handle conditional content

--- front_end/src/components/Film_Card.js
+++ front_end/src/components/Film_Card.js
@@ -3,6 +3,7 @@ import RatingsFunctions from "../services/RatingsFunctions"
 import ExternalServices from "../services/ExternalServices"
 import RatingComponent from "./Search_RatingComponent"
 import './Film_Card.css'
+import Wishlist_Button from "./Wishlist_Button"
 
 const Film_Card = ({film, wishlist, setWishlist, ratedFilms, setRatedFilms}) => {
 
@@ -17,17 +18,15 @@ const Film_Card = ({film, wishlist, setWishlist, ratedFilms, setRatedFilms}) => 
             {/* //  TITLE */}
             <h1 className='card_title'>{film.title}</h1>
             {/* //  YEAR */}
-            <p>({film.release_date.slice(0,4)})</p>
+            {film.release_date ? <p>({film.release_date.slice(0,4)})</p> : <></>}
             {/* //  AVERAGE RATING */}
             <p>Average rating: {film.vote_average}</p>
             {/* //  WISHLIST INDICATOR === MAKE INTO COMPONENT? */}
-            {(StatisticsFunctions.checkFilmOnList(film.id, wishlist)) ? 
-            <button className="wishlist_button" value={film._id} onClick={() => { RatingsFunctions.deleteFilmFromWishlist(film.id, setWishlist) }}>Remove from Wishlist</button> : 
-            <button className="wishlist_button" value={film._id} onClick={() => { RatingsFunctions.addToWishlist(film.id, wishlist, setWishlist) }}>Add to Wishlist</button>}
+            <Wishlist_Button film={film} wishlist={wishlist} setWishlist={setWishlist} ratedFilms={ratedFilms}></Wishlist_Button>
             {/* //  RATING COMPONENT */}
             <RatingComponent ratedFilms = {ratedFilms} setRatedFilms={setRatedFilms} filmId = {film.id} rating={RatingsFunctions.getRating(film.id, ratedFilms)}/>
             {/* //  AN EXPLICIT REMOVE FROM RATED FILMS button */}
-            <button className="ratings_button" value={film._id} onClick={() => RatingsFunctions.deleteFilmFromRatedList(film.id, setRatedFilms)}>Remove from rated films</button>
+            {(StatisticsFunctions.checkFilmOnList(film.id, ratedFilms)) ? <button className="ratings_button" value={film._id} onClick={() => RatingsFunctions.deleteFilmFromRatedList(film.id, setRatedFilms)}>Remove from rated films</button> : <></>}
             {/* //  FILM OVERVIEW */}
             <p>{film.overview}</p>
         </div>
